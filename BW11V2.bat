4c50a39f104364d4bf3462944dbec7c084f5934f
Zortos <65777760+zortos293@users.noreply.github.com>
2021-09-24 16:25:25 +0200

    Added Updated Bat

---
+++ BW11V2.bat
@@ -0,0 +1,60 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
+  set "DEL=%%a"
+)
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+cls
+title BW11V2
+echo -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+echo Bypass Windows 11 Version 2 (BW11V2)
+echo By Baka
+echo -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+echo Bypasses ALL Windows 11 requirements.
+echo Press any key to continue
+pause > nul
+cls
+call :colorEcho 40 "WARNING! Microsoft recommends not doing this. You've been warned." && echo.
+echo This script is intended if you're attempting to upgrade your build or to Windows 11 using the setting app.
+echo If so, upgrade normal, once the setup appears and it reports that you're not compatible. Close the setup.
+echo Then PRESS ENTER.
+pause > nul
+xcopy appraiserres.dll C:\$WINDOWS.~BT\Sources /Y
+cls
+echo Done.
+echo Press ENTER to exit.
+pause
+exit
+:colorEcho
+echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1i
